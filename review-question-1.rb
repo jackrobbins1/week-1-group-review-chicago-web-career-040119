7af1867661be4a2801bd3fce23e2f157f8c4b541
Jackson Robbins <jackrobbins1@gmail.com>
2019-04-08 09:52:58 -0500

    my answers to this discussion problem set

--- review-question-1.rb
+++ review-question-1.rb
@@ -1,5 +1,5 @@
 ## QUESTION 1
-
+require "pry"
 pokemon = [
   {
     "id": 1,
@@ -61,8 +61,96 @@ pokemon = [
 ]
 
 
+def open_pokemon
+  pokemon = [
+    {
+      "id": 1,
+      "name": "bulbasaur",
+      "base_experience": 64,
+      "height": 7,
+      "is_default": true,
+      "order": 1,
+      "weight": 69,
+      "abilities": [
+          {
+              "is_hidden": true,
+              "slot": 3,
+              "ability": {
+                  "name": "chlorophyll",
+                  "url": "http://pokeapi.co/api/v2/ability/34/"
+              }
+          }
+      ]
+  },
+  {
+    "id": 3,
+    "name": "venesaur",
+    "base_experience": 50,
+    "height": 10,
+    "is_default": true,
+    "order": 1,
+    "weight": 90,
+    "abilities": [
+        {
+            "is_hidden": true,
+            "slot": 3,
+            "ability": {
+                "name": "fire",
+                "url": "http://pokeapi.co/api/v2/ability/38/"
+            }
+        }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "pikachu",
+    "base_experience": 60,
+    "height": 4,
+    "is_default": true,
+    "order": 1,
+    "weight": 37,
+    "abilities": [
+        {
+            "is_hidden": true,
+            "slot": 3,
+            "ability": {
+                "name": "lightning",
+                "url": "http://pokeapi.co/api/v2/ability/30/"
+            }
+        }
+    ]
+  }
+  ]
+end
+
+def poke_40(pokemon)
+  pokemon.map { |poke_ind|
+    poke_ind.find {|keyz, valz|
+      binding.pry
+      keyz == "base_experience" && valz >= 40
+      binding.pry
+      poke_ind
+    }
+  }
+end
+
+def poke_50(pokemon)
+  pokemon.find { |poke_ind|
+    poke_ind[:"base_experience"] >= 40
+  }
+end
+
+poke_50(open_pokemon)
+binding.pry
 # How would you get the url for Bulbasaur's ability?
+  ## pokemon[0][:abilities][0][:ability][:url]
 # How would you return the first pokemon with base experience over 40?
+  ##
+  # def poke_50(pokemon)
+  #   pokemon.find { |poke_ind|
+  #     poke_ind[:"base_experience"] >= 40
+  #   }
+  # end
 # How would you return ALL OF THE pokemon with base experience over 40? (Gotta catch em all)
 # How would you return an array of all of the pokemon's names?
 # How would you determine whether or not the pokemon array contained any pokemon with a weight greater than 60?
